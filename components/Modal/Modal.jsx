45939d3588ab3d2eb5fb2050f74a833b8256e4e2
ekamuharyono <ekamuharyono02@gmail.com>
2022-08-14 12:22:56 +0700

    l

--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router'
 
+import { IoPersonOutline } from 'react-icons/io5'
+
 const Modal = (props) => {
 
   const router = useRouter()
@@ -77,7 +79,7 @@ const Modal = (props) => {
             <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"></div>
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"><IoPersonOutline /></div>
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <h3
                       className="text-lg leading-6 font-medium text-gray-900"
